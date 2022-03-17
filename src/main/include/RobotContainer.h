d1a93c7e33e247cdfd2865acb22c9b338e673c4f
Alistair Keiller <alistair@keiller.net>
2022-03-17 11:32:54 -0700

    move auto into RobotContrainer.h

--- src/main/include/RobotContainer.h
+++ src/main/include/RobotContainer.h
@@ -4,11 +4,11 @@
 #include <frc2/command/RunCommand.h>
 #include <frc2/command/FunctionalCommand.h>
 #include <frc2/command/InstantCommand.h>
+#include <frc2/command/SequentialCommandGroup.h>
 #include <frc2/command/button/JoystickButton.h>
 
 #include "subsystems/DrivetrainSubsystem.h"
 #include "subsystems/ClimberSubsystem.h"
-#include "commands/Auto.h"
 
 /**
  * This class is where the bulk of the robot should be declared.  Since
@@ -29,8 +29,6 @@ private:
   DrivetrainSubsystem m_drive;
   ClimberSubsystem m_climber;
 
-  Auto m_Auto{&m_drive};
-
   frc2::InstantCommand m_LeftClimberUp{[this]
                                        { m_climber.setLeftSpeed(.5); },
                                        {&m_climber}};
@@ -49,6 +47,23 @@ private:
   frc2::InstantCommand m_RightClimberDown{[this]
                                           { m_climber.setRightSpeed(-.5); },
                                           {&m_climber}};
+  
+  frc2::SequentialCommandGroup m_Auto{
+    frc2::FunctionalCommand(
+      [this] { m_drive.ResetEncoders(); },
+      [this] { m_drive.ArcadeDrive(AutoConstants::kAutoDriveSpeed, 0); },
+      [this] (bool interrupted) { m_drive.ArcadeDrive(0, 0); },
+      [this] { return m_drive.GetAverageEncoderDistanceInches() >= AutoConstants::kAutoDriveForwardDistanceInches; },
+      {&m_drive}
+      ),
+      frc2::FunctionalCommand(
+      [this] { },
+      [this] { m_drive.ArcadeDrive(-AutoConstants::kAutoDriveSpeed, 0); },
+      [this] (bool interrupted) { m_drive.ArcadeDrive(0, 0); },
+      [this] { return m_drive.GetAverageEncoderDistanceInches() <= 0; },
+      {&m_drive}
+      )
+  };
 
   void ConfigureButtonBindings();
 };
