176d828151265f4e9d4dcc9c5b228db74d7d8a02
Alistair Keiller <alistair@keiller.net>
2022-03-17 11:47:41 -0700

    remove unnecessary comments

--- src/main/include/RobotContainer.h
+++ src/main/include/RobotContainer.h
@@ -10,13 +10,6 @@
 #include "subsystems/DrivetrainSubsystem.h"
 #include "subsystems/ClimberSubsystem.h"
 
-/**
- * This class is where the bulk of the robot should be declared.  Since
- * Command-based is a "declarative" paradigm, very little robot logic should
- * actually be handled in the {@link Robot} periodic methods (other than the
- * scheduler calls).  Instead, the structure of the robot (including subsystems,
- * commands, and button mappings) should be declared here.
- */
 class RobotContainer
 {
 public:
