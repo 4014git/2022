4444bb05835be672536bbf1a0087247761ef329c
Alistair Keiller <alistair@keiller.net>
2022-03-20 12:24:19 -0700

    remove encoder

--- src/main/cpp/subsystems/ClimberSubsystem.cpp
+++ src/main/cpp/subsystems/ClimberSubsystem.cpp
@@ -4,8 +4,7 @@ using namespace ClimberConstants;
 
 ClimberSubsystem::ClimberSubsystem()
     : m_leftMotor{kLeftMotor, rev::CANSparkMaxLowLevel::MotorType::kBrushless},
-      m_rightMotor{kRightMotor, rev::CANSparkMaxLowLevel::MotorType::kBrushless},
-      m_leftEncoder{m_leftMotor.GetEncoder()}, m_rightEncoder{m_rightMotor.GetEncoder()}
+      m_rightMotor{kRightMotor, rev::CANSparkMaxLowLevel::MotorType::kBrushless}
 {
     m_leftMotor.SetInverted(kLeftnverted);
     m_rightMotor.SetInverted(kRightInverted);
@@ -28,20 +27,4 @@ void ClimberSubsystem::setSpeed(double speed)
 {
     m_leftMotor.Set(speed);
     m_rightMotor.Set(speed);
-}
-
-void ClimberSubsystem::ResetLeftEncoder()
-{
-    m_leftEncoder.SetPosition(0);
-}
-
-void ClimberSubsystem::ResetRightEncoder()
-{
-    m_rightEncoder.SetPosition(0);
-}
-
-void ClimberSubsystem::ResetEncoders()
-{
-    m_leftEncoder.SetPosition(0);
-    m_rightEncoder.SetPosition(0);
 }
--- src/main/include/subsystems/ClimberSubsystem.h
+++ src/main/include/subsystems/ClimberSubsystem.h
@@ -14,17 +14,8 @@ class ClimberSubsystem : public frc2::SubsystemBase
     void setLeftSpeed(double speed);
     void setRightSpeed(double speed);
     void setSpeed(double speed);
-    void ResetLeftEncoder();
-    void ResetRightEncoder();
-    void ResetEncoders();
-    double getLeftEncoderDistance();
-    double getRightEncoderDistance();
-    double getAverageEncoderDistance();
 
   private:
     rev::CANSparkMax m_leftMotor;
     rev::CANSparkMax m_rightMotor;
-
-    rev::SparkMaxRelativeEncoder m_leftEncoder;
-    rev::SparkMaxRelativeEncoder m_rightEncoder;
 };
